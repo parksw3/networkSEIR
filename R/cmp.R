774807e86304a5ba10374c2dc9df890fdf4a9694
parksw3 <parksw3@mcmaster.ca>
2016-10-11 18:34:53 -0400

    seems to be working for now

--- R/cmp.R
+++ R/cmp.R
@@ -3,11 +3,31 @@ source("model.R")
 cmpGraph <- read.table("../data/CA-CondMat.txt") ##importing condensed matter physics data
 cmpGraph <- graph.data.frame(cmpGraph)
 cmpGraph <- as.undirected(cmpGraph)
+cmpGraph <- delete_vertex_attr(cmpGraph, "name")
 
 pars <- c(
-    beta = 0.01,
+    beta = 0.15,
     sigma = 1/2,
-    gamma = 1
+    gamma = 1,
+    I0 = 10
 )
 
-r <- seir.gillespie(cmpGraph, pars, verbose = TRUE)
+set.seed(101)
+cmpGraph.sim <- seir.gillespie(cmpGraph, pars, verbose = TRUE)
+
+epi <- cmpGraph.sim$result
+matplot(epi[,1], epi[,-1], type = "l")
+
+gen <- cmpGraph.sim$generation
+hist(gen, breaks = c(0:25))
+mean(gen)
+var(gen)
+
+set.seed(101)
+r <- seir.gillespie(g, pars)
+mean(r$generation)
+var(r$generation)
+hist(r$generation, breaks = c(0:30))
+
+r2 <- r$result
+
--- R/model.R
+++ R/model.R
@@ -6,27 +6,33 @@ seir.gillespie <- function(g, parameters,
     
     with(as.list(parameters),{
         N <- length(V(g))
-        state <- rep("S", N)
         
-        connected <- as(g[], "sparseMatrix")
-        connected <- unname(connected)
-        ## connected <- as.matrix(g[])
-        max.edge <- which.max(colSums(connected))
-        state[max.edge] <- "I"
+        state <- rep("S", N)
+        rate <- rep(0, N)
+        
+        infected <- sample(N, I0)
+        state[infected] <- "I"
+        
+        inf.partner <- rep(0, N)
+        
+        for(i in infected){
+            inf.partner[neighbors(g, i)] <- inf.partner[neighbors(g,i)] + 1
+        }
+        
+        rate <- beta * inf.partner
+        
+        rate[infected] <- gamma
         
         inf.t <- rep(NA, N)
-        inf.t[max.edge] <- 0
+        inf.t[infected] <- 0
         infector <- rep(NA, N)
-        inf.order <- rep(NA, 0)
+        inf.order <- rep(NA, N)
+        inf.order[1:I0] <- infected
         
-        inf <- as.numeric(state == "I")
         
-        rate.mat <- beta * inf * connected
-        rate.mat[max.edge,max.edge] <- gamma
+        rsum <- sum(rate)
         
-        rsum <- sum(rate.mat)
-        
-        t <- 0; S <- N-1; E <- 0; I <- 1; R <- 0
+        t <- 0; S <- N-I0; E <- 0; I <- I0; R <- 0
         
         output <- list()
         
@@ -39,51 +45,86 @@ seir.gillespie <- function(g, parameters,
             next.t <- rexp(1, rsum)
             t <- t + next.t
             
-            # prob <- c(rate.mat)
-            
-            prob.ind <- which(rate.mat != 0, arr.ind = TRUE)
-            prob <- rate.mat[prob.ind]
-            
-            next.event <- which(rmultinom(1, size = 1, prob = prob) == 1)
-            change.ind <- prob.ind[next.event,]
-            vchange <- change.ind[2]
+            vchange <- which(rmultinom(1, size = 1, prob = rate) == 1)
             
             if(state[vchange] == "S"){
                 state[vchange] <- "E"
-                rate.mat[,vchange] <- 0
-                rate.mat[vchange,vchange] <- sigma
-                infector[vchange] <- change.ind[1]
-                    
+                rate[vchange] <- sigma
+                
+                n.V <- neighbors(g,vchange)
+                n.V.I <- n.V[state[n.V] == "I"]
+                infector[vchange] <- n.V.I[sample(length(n.V.I), 1)]
+                
                 S <- S - 1; E <- E + 1
                 
                 inf.order[N - S] <- vchange
             }else if(state[vchange] == "E"){
                 state[vchange] <- "I"
-                rate.mat[vchange,(state == "S")] <- beta
-                rate.mat[vchange,vchange] <- gamma
+                
+                n.V <- neighbors(g,vchange)
+                n.V.S <- n.V[state[n.V] == "S"]
+                inf.partner[n.V.S] <- inf.partner[n.V.S] + 1
+                rate[n.V.S] <- beta * inf.partner[n.V.S]
+                rate[vchange] <- gamma
+                
                 inf.t[vchange] <- t
                 
                 E <- E - 1; I <- I + 1
             }else if(state[vchange] == "I"){
                 state[vchange] <- "R"
-                rate.mat[vchange,] <- 0
+                n.V <- neighbors(g, vchange)
+                n.V.S <- n.V[state[n.V] == "S"]
+                inf.partner[n.V.S] <- inf.partner[n.V.S] - 1
+                rate[n.V.S] <- beta * inf.partner[n.V.S]
+                rate[vchange] <- 0
                 
                 I <- I - 1; R <- R + 1
             }
             
-            rsum <- sum(rate.mat)
+            rsum <- sum(rate)
             output[[i]] <- data.frame(t, S, E, I, R)
-            if(verbose){
+            if(verbose & i %% 1000 == 0){
                 print(output[[i]])
             }
         }
         
         output <- do.call('rbind', output)
         
+        R0.gen <- list()
+        R0.gen[[1]] <- which(infector %in% infected)
+        j <- 1
+        repeat{
+            j <- j + 1
+            R0.gen[[j]] <- which(infector %in% R0.gen[[j-1]])
+            if(length(R0.gen[[j-1]]) > 75){
+                break
+            }
+        }
+        R0.vec <- unlist(lapply(R0.gen, length))
+        R0.generation <- sum(R0.vec[-1])/sum(R0.vec[-length(R0.vec)])
+        
+        r.data <- data.frame(
+            tvec <- output[,1],
+            tot <- rowSums(output[,c("I", "R")])
+        )
+        r.lm <- lm(log(tot)~tvec, data = r.data, tot > 200 & tot < 400)
+        r <- unname(coef(r.lm)[2])
+        
+        mean.g <- mean(degree(g))
+        var.g <- var(degree(g))
+        kappa <- var.g/mean.g + mean.g - 1
+        
+        R0.Markov <- unname((gamma+r)/(gamma * sigma/(sigma + r) + r/kappa))
+        
+        g.summary <- list(R0.generation = R0.generation,
+                        R0.Markov = R0.Markov,
+                        r = r)
+        
         generation <- inf.t - inf.t[infector]
         generation <- generation[!is.na(generation)]
         
         return(list(result = output,
-                    generation = generation))  
+                    generation = generation,
+                    summary = g.summary))  
     })
 }
