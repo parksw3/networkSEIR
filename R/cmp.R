6c0d063b0d819604feeeee07df799ae16a78dd9d
parksw3 <parksw3@mcmaster.ca>
2016-10-11 19:04:18 -0400

    for now

--- R/cmp.R
+++ R/cmp.R
@@ -6,28 +6,35 @@ cmpGraph <- as.undirected(cmpGraph)
 cmpGraph <- delete_vertex_attr(cmpGraph, "name")
 
 pars <- c(
-    beta = 0.15,
-    sigma = 1/2,
-    gamma = 1,
+    beta = 0.03,
+    sigma = 1/9,4,
+    gamma = 1/5,
     I0 = 10
 )
 
-set.seed(101)
-cmpGraph.sim <- seir.gillespie(cmpGraph, pars, verbose = TRUE)
+## set.seed(101)
+## cmpGraph.sim <- seir.gillespie(cmpGraph, pars, verbose = TRUE)
+
+## save("cmpGraph.sim", file = "cmpSim1.rda")
+
+load("compSim1.rda")
+
+print(cmpGraph.sim$summary)
 
 epi <- cmpGraph.sim$result
-matplot(epi[,1], epi[,-1], type = "l")
+matplot(epi[,1], epi[,"I"], type = "l")
 
 gen <- cmpGraph.sim$generation
-hist(gen, breaks = c(0:25))
+hist(gen, breaks = c(0:120))
 mean(gen)
 var(gen)
 
-set.seed(101)
-r <- seir.gillespie(g, pars)
-mean(r$generation)
-var(r$generation)
-hist(r$generation, breaks = c(0:30))
-
-r2 <- r$result
-
+# Homogeneous case
+#
+# set.seed(101)
+# r <- seir.gillespie(g, pars)
+# mean(r$generation)
+# var(r$generation)
+# hist(r$generation, breaks = c(0:30))
+# 
+# r2 <- r$result
