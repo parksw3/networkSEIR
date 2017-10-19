54a53a23d397986130e6a6e651c498b680917aa0
parksw3 <parksw3@mcmaster.ca>
2017-10-19 01:39:30 -0400

    last commit before i go to bed. poster tomorrow

--- misc/example.R
+++ misc/example.R
@@ -1,24 +1,23 @@
 library(igraph)
 source("../R/heap.R")
 
-## a scale-free graph
-g <-sample_pa(n=500, power=1, m=20,  directed=F)
+cmpGraph <- read.table("../data/CA-CondMat.txt")
+cmpGraph <- graph.data.frame(cmpGraph)
+cmpGraph <- as.undirected(cmpGraph)
+cmpGraph <- delete_vertex_attr(cmpGraph, "name")
 
 ## randomly selected beta and gamma values. 
 ## Note that beta here is the contact rate between two individuals. 
-## So the homogeneous (everyone is connected to everyon) equivalent beta would be beta * (number of verticies-1)  
-## We need this homogeneous equivalent beta when calculating the spatial generation distribution
-## Later, we want to fix beta for each individual and distribute contact rates among neighbours
 beta <- 0.4
 gamma <- 3
 
-v_dist <- degree(g)
+v_dist <- degree(cmpGraph)
 
 mu_v <- mean(v_dist)
 
 kappa <- var(v_dist)/mu_v + mu_v - 1
 
-res <- seir.heap(g, beta, gamma, seed=101)
+system.time(res <- seir.heap(cmpGraph, beta, gamma, seed=101))
 
 ## what the epidemic looks like
 plot(res$data, type="l")
@@ -29,10 +28,12 @@ curve(gamma*exp(-gamma*x), col=3, add=TRUE) ## looks pretty good
 
 ## calculated by considering only the first contacts
 plot(density(res$t_infected-res$t_infected[res$infected_by], na.rm=TRUE), main="spatial generation") 
-curve(gamma*exp(-gamma*x), col=2, add=TRUE)
-curve((beta*(length(V(g))-1)/kappa+gamma)*exp(-(beta*(length(V(g))-1)/kappa+gamma)*x), col=3, add=TRUE)
-## I think homogeneous equivalent beta has to appear here
-## I would say that this looks pretty good
+lines(density(unlist(res$forward_generation)), main="intrinsic generation", col=2, lty=2) 
+legend(2, 3, c("spatial", "intrinsic"), col=c(1, 2), lty=c(1, 2))
+## It's not so clear what the spatial generation equation should be... I need to think about that a bit more
+## I feel like Trapman doesn't quite work and we need to account for the fact that individuals with more neighbours
+## will have higher rate of contact in this model
+## we can at least see that the spatial generation is shorter than the intrinsic
 
 ## just checking that the infectious period from the simulation matches the expected distribution
 plot(density(res$t_recovered-res$t_infected, na.rm=TRUE), col=2, main="infectious period")
