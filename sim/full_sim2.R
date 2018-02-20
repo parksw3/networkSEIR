78ae582259810945c0735ae0b0020b206906bdda
parksw3 <parksw3@mcmaster.ca>
2018-02-20 01:38:09 -0500

    pushed the wrong version

--- sim/full_sim2.R
+++ sim/full_sim2.R
@@ -9,8 +9,8 @@ reslist <- vector('list', nsim)
 i <- 1
 while (i <= nsim) {
     print(i)
-    rr <- seir.full(N, beta/N, sigma, gamma, I0 = 1, imax=10000)
-    if(nrow(rr$data) > 8000) {
+    rr <- seir.full(N, beta/N, sigma, gamma, I0 = 1, imax=6000)
+    if(nrow(rr$data) > 5000) {
         reslist[[i]] <- rr
         i <- i+1
     }
